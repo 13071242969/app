0c0582fc3665de61f9bf394c7b4ff36da3323b4a
start <1170491914@qq.com>
2016-04-29 09:57:26 +0800

    Update MainActivity.java

--- app/src/main/java/com/ccs/android/app/MainActivity.java
+++ app/src/main/java/com/ccs/android/app/MainActivity.java
@@ -10,7 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    int aasd；
+    int qwe；
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
